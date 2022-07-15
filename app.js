9e19c181e6981fdb0285ae832c626f93764cfced
programmermohaimin <you@example.com>
2022-07-15 18:41:36 +0600

    almost done

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ window.addEventListener('scroll', function () {
     
     header.classList.toggle('sticky', this.window.scrollY > 0);
 })
+
+let menu = document.querySelector('#menu-icon');
+let nav = document.querySelector('#nav-list');
+
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('active');
+}
